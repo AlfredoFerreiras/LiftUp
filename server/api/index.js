2bdce4195f46de66dd2953a338e0d34ec41d4bfd
AlfredoFerreiras <alfredoferreiras@yahoo.com>
2024-03-20 00:35:19 -0400

    Fixed error for .openai

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 module.exports = router;
 
-const openaiRoutes = require("./openai");
 const companyRoutes = require("./company");
 const googleRoutes = require("./googleai");
 
 router.use("/users", require("./users"));
-router.use("/openai", openaiRoutes);
+
 router.use("/companies", companyRoutes);
 router.use("/googleai", googleRoutes);
 router.use((req, res, next) => {
